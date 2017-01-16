10fa42fcea7054b2b5414b63f6969db15a86e700
Timo Beckers <timo@incline.eu>
2017-01-16 11:25:33 +0100

    lp-gw/base - consolidate to_hex and split_url filter into libraries/

--- libraries/filter_plugins/strings.py
+++ libraries/filter_plugins/strings.py
@@ -1,8 +1,11 @@
-# Filters for lp-gw
-
 from ansible import errors
 
+# Return the hostname from a URL
+# TODO: strip http(s) from the start of the string
+def split_url(s, char='/'):
+  return s.split(char, 1)[0]
 
+# Convert an integer to a hex value padded to a given width
 def to_hex(a, width=4):
   if not isinstance(a, int):
     raise errors.AnsibleFilterError("to_hex can only be called on integers (got '{}')".format(a))
@@ -12,11 +15,11 @@ def to_hex(a, width=4):
 
   return format(a, '0{}x'.format(width))
 
-
 class FilterModule(object):
-  ''' lp-gw hex int/string operations '''
+  ''' ansible-lanparty string filters '''
 
   def filters(self):
     return {
+      'split_url': split_url,
       'to_hex': to_hex
     }
